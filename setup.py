90a5026ffeb75d83018296f427e1e13416cdbe5d
Sergio Troiano <sergio.troiano@adevinta.com>
2023-09-10 23:23:06 +0200

    Indentation in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 import inspect
 import os
 import sys
+from pathlib import Path
 
-from setuptools import find_packages, setup, Extension
+from setuptools import Extension, find_packages, setup
 from setuptools.command.test import test as TestCommand
 
 __location__ = os.path.join(
@@ -24,7 +25,7 @@ def get_install_requirements(path):
 setup(
     name="pagecache_ttl",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     version=version,
     description="Monitor the minimal TTL of cached pages by the OS",
     author="Datastreaming",
@@ -35,6 +36,6 @@ setup(
     zip_safe=False,
     entry_points={"console_scripts": ["pagecache=pagecache.cli:main"]},
     ext_modules=[
-          Extension('cache', ['pagecache/src/cachemodule.c']),
-      ],
+        Extension("cache", ["pagecache/src/cachemodule.c"]),
+    ],
 )
